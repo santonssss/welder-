13a14ba775828934c39a9309571b723c81f19034
santonssss <sarvarkalmuratov370@gmail.com>
2025-03-20 23:04:18 +0500

    remove ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "@pages/Index";
-import { Toaster } from "@components/ui/toaster";
-import { Toaster as Sonner } from "@components/ui/sonner";
 const App = () => {
   return (
     <BrowserRouter>
-      <Toaster />
-      <Sonner />
       <Routes>
         <Route path="/" element={<Index />} />
       </Routes>
